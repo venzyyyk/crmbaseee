a224c7f3f488563ab6958d6b282a33d6019f8852
venzyyyk <cubeday2008@gmail.com>
2026-04-22 21:11:13 +0300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,9 +10,9 @@ const app = express();
 const upload = multer({ storage: multer.memoryStorage(), limits: { fileSize: 10 * 1024 * 1024 } });
 
 app.use(cors({
-  origin: '*', 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  origin: '*',
+  methods: '*',
+  allowedHeaders: '*'
 }));
 app.use(express.json());
 
